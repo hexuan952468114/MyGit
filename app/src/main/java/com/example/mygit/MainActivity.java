c6ec89aa30640c3c054c2f24e3bec13689cc728c
Administrator <550389302@qq.com>
2019-05-16 15:54:28 +0800

    k;lk;lk;l;

--- app/src/main/java/com/example/mygit/MainActivity.java
+++ app/src/main/java/com/example/mygit/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.mygit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Text userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //可考虑急急急急急急急急急急急急急急急
+        userName=this.findViewById(R.id.usernames);
+        ((View) userName).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return false;
+            }
+        });
+    }
+    public void MOTH(){
+        
     }
 }
